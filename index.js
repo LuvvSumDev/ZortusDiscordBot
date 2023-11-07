8e929fd51f297a7c1a80e8ad64d591e65288ef88
LuvvSum <148398076+LuvvSumDev@users.noreply.github.com>
2023-11-07 16:00:29 +0100

    Deleted dumb and unused variables in index.js.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits, Events, ChannelType, PermissionFlagsBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Embed } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const config = require('./config.json');
-const { createTranscript } = require('discord-html-transcripts');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
