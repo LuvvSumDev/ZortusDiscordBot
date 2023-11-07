e0ce81fe8a57a3cfb26b1a7d2b20ac3ddf064227
LuvvSum <148398076+LuvvSumDev@users.noreply.github.com>
2023-11-07 16:58:23 +0100

    Suggestion system added.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, Events, EmbedBuilder, ChannelType } = require('discord.js');
 const config = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
@@ -35,5 +35,28 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+
+client.on(Events.MessageCreate, async message => {
+	if (message.author.bot) return;
+
+	const suggestionChannel = client.channels.cache.get(config.suggestionChannel);
+
+	if (message.channelId == suggestionChannel) {
+		const suggestionEmbed = new EmbedBuilder()
+			.setAuthor({ name: 'Zortus Roleplay - Suggesties', iconURL: 'https://cdn.discordapp.com/attachments/1169103447606431834/1169103713529516093/zortus.png?ex=65542fae&is=6541baae&hm=3e9525adf794493afa093e40feabe8b43f0f95372d0d9f93f1f13fc3b16829d2&' })
+			.setColor(0xFFFFFF)
+			.setThumbnail('https://cdn.discordapp.com/attachments/1169103447606431834/1169103713529516093/zortus.png?ex=65542fae&is=6541baae&hm=3e9525adf794493afa093e40feabe8b43f0f95372d0d9f93f1f13fc3b16829d2&')
+			.setDescription(`**Suggestie vorzonden door:**\n<@${message.author.id}> (${message.author.username})\n\n**Ingestuurde suggestie:**\n${message.content}`)
+
+		const suggestion = await suggestionChannel.send({ embeds: [suggestionEmbed] });
+
+		suggestion.react('👍');
+		suggestion.react('👎');
+
+		await suggestion.startThread({ name: 'Suggestie Discussie', autoArchiveDuration: 60, type: ChannelType.PublicThread });
+
+		message.delete();
+	}
+})
 
 client.login(config.token);
